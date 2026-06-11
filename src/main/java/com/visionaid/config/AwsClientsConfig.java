a5245336f7d9135b988957b371424c2f9aabf46d
RishitaSadhwani2 <rsadhwa1@asu.edu>
2026-06-10 19:47:54 -0700

    Use default AWS credentials provider for deployment

--- src/main/java/com/visionaid/config/AwsClientsConfig.java
+++ src/main/java/com/visionaid/config/AwsClientsConfig.java
@@ -3,6 +3,7 @@ package com.visionaid.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.polly.PollyClient;
@@ -22,8 +23,8 @@ public class AwsClientsConfig {
     }
 
     @Bean
-    public ProfileCredentialsProvider profileCredentialsProvider() {
-        return ProfileCredentialsProvider.create(AWS_PROFILE_NAME);
+    public DefaultCredentialsProvider profileCredentialsProvider() {
+        return DefaultCredentialsProvider.create();
     }
 
     @Bean
